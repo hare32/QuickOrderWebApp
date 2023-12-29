664eb66e1f4e3c31bbf73d24b0b607103a723e4c
hare32 <234694@edu.p.lodz.pl>
2023-12-29 18:03:54 +0100

    na 3 Final 1.1

--- src/backend/routes/orders.js
+++ src/backend/routes/orders.js
@@ -7,43 +7,120 @@ const {isInt, isDate} = require("validator");
 // Pobierz wszystkie zamówienia
 router.get('/', async (req, res) => {
     try {
-        const orders = await knex.select('*').from('orders');
-        res.json(orders);
+        const orders = await knex('orders')
+            .leftJoin('order_items', 'orders.id', 'order_items.order_id')
+            .leftJoin('products', 'order_items.product_id', 'products.id')
+            .select('orders.*', 'order_items.quantity', 'products.name as product_name', 'products.id as product_id')
+            .orderBy('orders.id');
+
+        // Grupowanie produktów dla każdego zamówienia
+        const groupedOrders = orders.reduce((acc, order) => {
+            if (!acc[order.id]) {
+                acc[order.id] = {
+                    id: order.id,
+                    user_name: order.user_name,
+                    email: order.email,
+                    phone: order.phone,
+                    status_id: order.status_id,
+                    confirmation_date: order.confirmation_date,
+                    products: []
+                };
+            }
+
+            if (order.product_id) {
+                acc[order.id].products.push({
+                    product_id: order.product_id,
+                    product_name: order.product_name,
+                    quantity: order.quantity
+                });
+            }
+
+            return acc;
+        }, {});
+
+        res.json(Object.values(groupedOrders));
     } catch (error) {
         res.status(500).send(error);
     }
 });
+
 
 // Pobierz zamówienie po ID
 router.get('/:id', async (req, res) => {
     try {
-        const order = await knex('orders').where('id', req.params.id).first();
-        if (order) {
-            res.json(order);
-        } else {
-            res.status(404).send('Order not found');
+        const orderDetails = await knex('orders')
+            .leftJoin('order_items', 'orders.id', 'order_items.order_id')
+            .leftJoin('products', 'order_items.product_id', 'products.id')
+            .select('orders.*', 'order_items.quantity', 'products.name as product_name', 'products.id as product_id')
+            .where('orders.id', req.params.id);
+
+        if (orderDetails.length === 0) {
+            return res.status(404).send('Order not found');
         }
+
+        const order = {
+            ...orderDetails[0],
+            products: orderDetails.map(item => ({
+                product_id: item.product_id,
+                product_name: item.product_name,
+                quantity: item.quantity
+            }))
+        };
+
+        res.json(order);
     } catch (error) {
         res.status(500).send(error);
     }
 });
 
+
 // Pobierz zamówienia dla konkretnej nazwy użytkownika
 router.get('/user/:username', async (req, res) => {
     try {
-        const orders = await knex('orders').where('user_name', req.params.username);
-        res.json(orders);
+        const userOrders = await knex('orders')
+            .leftJoin('order_items', 'orders.id', 'order_items.order_id')
+            .leftJoin('products', 'order_items.product_id', 'products.id')
+            .select('orders.*', 'order_items.quantity', 'products.name as product_name', 'products.id as product_id')
+            .where('orders.user_name', req.params.username)
+            .orderBy('orders.id');
+
+        const groupedOrders = userOrders.reduce((acc, order) => {
+            if (!acc[order.id]) {
+                acc[order.id] = {
+                    id: order.id,
+                    user_name: order.user_name,
+                    email: order.email,
+                    phone: order.phone,
+                    status_id: order.status_id,
+                    confirmation_date: order.confirmation_date,
+                    products: []
+                };
+            }
+
+            if (order.product_id) {
+                acc[order.id].products.push({
+                    product_id: order.product_id,
+                    product_name: order.product_name,
+                    quantity: order.quantity
+                });
+            }
+
+            return acc;
+        }, {});
+
+        res.json(Object.values(groupedOrders));
     } catch (error) {
         res.status(500).send(error);
     }
 });
+
 
 // Dodaj nowe zamówienie
 router.post('/', [
     body('user_name').notEmpty().withMessage('User name cannot be empty'),
     body('email').isEmail().withMessage('Invalid email format'),
     body('phone').isMobilePhone().withMessage('Invalid phone number format'),
-    body('status_id').custom(value => isInt(value, { min: 1, max: 4 })).withMessage('Status ID must be between 1 and 4'),
+    body('status_id').isInt({ min: 1, max: 4 }).withMessage('Status ID must be between 1 and 4'),
     body('confirmation_date').optional().custom(value => !value || isDate(value)).withMessage('Invalid date format'),
     body('products').isArray().withMessage('Products must be an array'),
     body('products.*.product_id').isInt({ gt: 0 }).withMessage('Product ID must be a positive integer'),
@@ -128,17 +205,48 @@ router.patch('/:id', async (req, res) => {
 // Pobierz zamówienia z określonym stanem
 router.get('/status/:statusId', async (req, res) => {
     try {
-        const statusId = req.params.status_id;
-        const orders = await knex('orders').where('status_id', statusId);
-        if (orders.length > 0) {
-            res.json(orders);
-        } else {
-            res.status(404).send('No orders found for the given status');
+        const statusOrders = await knex('orders')
+            .leftJoin('order_items', 'orders.id', 'order_items.order_id')
+            .leftJoin('products', 'order_items.product_id', 'products.id')
+            .select('orders.*', 'order_items.quantity', 'products.name as product_name', 'products.id as product_id')
+            .where('orders.status_id', req.params.statusId)
+            .orderBy('orders.id');
+
+        // Sprawdzenie, czy zostały znalezione jakiekolwiek zamówienia
+        if (statusOrders.length === 0) {
+            return res.status(404).send('No orders found for the given status');
         }
+
+        const groupedOrders = statusOrders.reduce((acc, order) => {
+            if (!acc[order.id]) {
+                acc[order.id] = {
+                    id: order.id,
+                    user_name: order.user_name,
+                    email: order.email,
+                    phone: order.phone,
+                    status_id: order.status_id,
+                    confirmation_date: order.confirmation_date,
+                    products: []
+                };
+            }
+
+            if (order.product_id) {
+                acc[order.id].products.push({
+                    product_id: order.product_id,
+                    product_name: order.product_name,
+                    quantity: order.quantity
+                });
+            }
+
+            return acc;
+        }, {});
+
+        res.json(Object.values(groupedOrders));
     } catch (error) {
         res.status(500).send(error);
     }
 });
+
 
 
 module.exports = router;
